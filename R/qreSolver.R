39e77ddcfd29483eec8fb75f2d50eb8d1d71d6ab
Leo Oh <soh24@tamu.edu>
2025-11-16 16:00:18 -0600

    Implement qre model

---
+++ R/qreSolver.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param lambda
+#' @param payoffs_p1
+#' @param payoffs_p2
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+solve_qre <- function(lambda, payoffs_p1, payoffs_p2) {
+
+}
+
