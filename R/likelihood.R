df4b58372bdd6d068cc6ae1557a128f8dd22d57e
Leo Oh <soh24@tamu.edu>
2025-11-18 15:51:35 -0600

    Calculate log likelihood using observations from real data

--- R/likelihood.R
+++ R/likelihood.R
@@ -1,3 +1,11 @@
 log_likelihood <- function(observed_freqs, qre_probs) {
 
+  n <- observed_freqs$n
+
+  # Log likelihood: sum(n_i * log(p_i))
+
+  ll_p1 <- sum((observed_freqs$p1 * n) * log(qre_probs$p1))
+  ll_p2 <- sum((observed_freqs$p2 * n) * log(qre_probs$p2))
+
+  ll_p1 + ll_p2
 }
