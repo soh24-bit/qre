c610f6a52df0474aa76fb08e10d49008eddc1345
Leo Oh <soh24@tamu.edu>
2025-11-18 16:15:17 -0600

    Add negative-liklihood function

--- R/likelihood.R
+++ R/likelihood.R
@@ -1,4 +1,4 @@
-log_likelihood <- function(observed_freqs, qre_probs) {
+ll <- function(observed_freqs, qre_probs) {
 
   n <- observed_freqs$n
 
@@ -8,4 +8,19 @@ log_likelihood <- function(observed_freqs, qre_probs) {
   ll_p2 <- sum((observed_freqs$p2 * n) * log(qre_probs$p2))
 
   ll_p1 + ll_p2
+}
+
+nll <- function(lambda, observed_freqs, payoffs_p1, payoffs_p2) {
+
+  # lambda must be non-negative
+  if (lambda < 0) {
+    return(Inf)
+  }
+
+  # Solve for QRE given lambda
+  qre_probs <- solve_qre(lambda, payoffs_p1, payoffs_p2)
+
+  # Return negative log-likelihood
+  -ll(observed_freqs, qre_probs)
+
 }
