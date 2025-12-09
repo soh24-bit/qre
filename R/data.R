3ca582b599f875bf59ccab9181ab10427101047c
Leo Oh <soh24@tamu.edu>
2025-12-09 13:31:05 -0600

    Add Roxygen skeleton for example data (remove warning)

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' @title Example prisoner's dilemma data from Harvard
+#'
+#' @description
+#' This data is cleaned so each column represents player 1 and 2's choices. There are 16880 observations.
+#'
+#' @format A data frame:
+#' \describe{
+#'   \item{p1_choice}{Player 1 choices}
+#'   \item{p2_choice}{Player 2 choices}
+#' }
+"example_data"
